1e312e6aa60c14ec40c6686adf785e488bfb425c
ProWeb4All <proweb4all@narod.ru>
2020-04-27 21:39:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.use((req, res, next) => {
+  res.sendFile('/index.html')
+}) 
+
+// app.listen(PORT)
+
+
+try {
+  app.listen(PORT, () => {
+    console.log(`Server was running on port ${PORT}...`)
+  })
+} catch (e) {
+  console.log(e)
+}
