b2460d658e32d85043829d9b174bd2b362cf4600
ProWeb4All <proweb4all@narod.ru>
2020-05-02 23:27:19 +0300

    add DB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const sequelize = require('./utils/database')
 const todoRoutes = require('./routes/todo')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -14,11 +15,16 @@ app.use((req, res, next) => {
 
 // app.listen(PORT)
 
-
-try {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`)
-  })
-} catch (e) {
-  console.log(e)
+async function start() {
+  try {
+    await sequelize.sync()
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}...`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
+
+
+start()
